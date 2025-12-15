3574df28c9fac479614a824b2e32e199e8a3590f
Alvarez Bermudez <68402612+Alvarez-Bermudez@users.noreply.github.com>
2025-12-15 01:53:13 -0300

    Implement JwtAuthGuard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '7d' },
@@ -13,6 +16,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- fetcher/index.js
+++ fetcher/index.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:3000/";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3YzMyNmY3Zi1mMTNjLTRjZDItYTgwYS1mOTRiZmU5ZjE5ZGQiLCJlbWFpbCI6Impob25AZ21haWwuY29tIiwiaWF0IjoxNzY1NzcyMjcyLCJleHAiOjE3NjYzNzcwNzJ9.uiGnhcl1xcEmAXYxHgJ9MhNYr-ahuPts2fvuWxGsK50";
+
 async function main() {
   try {
+    const response = await axios.get(`${baseUrl}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     console.log(response.data);
   } catch (e) {
     console.error("Error:", e.response?.data || e.message);
