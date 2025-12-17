a9c1e324ea8d2e49709c50f975081367e0814830
Alvarez Bermudez <68402612+Alvarez-Bermudez@users.noreply.github.com>
2025-12-17 00:00:41 -0300

    Implement GET pets/:id/diets/history/:menu_id

--- backend/src/diets/diets.service.ts
+++ backend/src/diets/diets.service.ts
@@ -6,12 +6,6 @@ export class DietsService {
   constructor(private prisma: PrismaService) {}
 
   async getHistory(userId: string, petId: string) {
-    const pet = await this.prisma.pet.findUnique({
-      where: { id: petId, userId },
-      select: { userId: true },
-    });
-    if (!pet) throw new NotFoundException('Pet not found'); // Esto garantiza tambien que algun usuario no pueda acceder a la informacion de cualquier mascota, en caso de que esta pertenezca a otro usuario diferente del mismo
-
     const history = await this.prisma.menuHistory.findMany({
       where: { petId },
       select: { id: true, petId: true, date: true },
@@ -20,5 +14,11 @@ export class DietsService {
     return history;
   }
 
-  async getMenuById(userId: string, petId: string, menuId: string) {}
+  async getMenuById(userId: string, petId: string, menuId: string) {
+    const menu = await this.prisma.menuHistory.findUnique({
+      where: { id: menuId, petId },
+    });
+
+    return menu;
+  }
 }
