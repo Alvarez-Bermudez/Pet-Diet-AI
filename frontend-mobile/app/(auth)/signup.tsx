6b798197fcf87a0939fd2309c8b8db0e2a4be14b
Alvarez Bermudez <68402612+Alvarez-Bermudez@users.noreply.github.com>
2025-12-21 04:51:29 -0300

    Anadir KeyboardAvoidingView a Sign up page

--- frontend-mobile/app/(auth)/signup.tsx
+++ frontend-mobile/app/(auth)/signup.tsx
@@ -1,7 +1,14 @@
 import { TextInputWithIcon } from "@/components/TextInput";
 import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 import profileIcon from "../../assets/images/Customer.png";
 import lockIcon from "../../assets/images/Customer.png";
 import emailIcon from "../../assets/images/Email.png";
@@ -15,96 +22,102 @@ export default function SignUpPage() {
   const [confirmPasssword, setConfirmPassword] = useState<string>();
 
   return (
-    <SafeAreaView className="relative flex-1 px-5 py-5 justify-center items-center bg-background gap-[30px]">
-      <View className="gap-1 items-center">
-        <Text
-          className="font-medium text-h3 text-textPrimary "
-          style={{ fontFamily: "Poppins_500Medium" }}
-        >
-          Sign up
-        </Text>
-        <Text
-          className="text-small text-textPrimary"
-          style={{ fontFamily: "Inter_400Regular" }}
-        >
-          Create an account with your info
-        </Text>
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      className="h-full"
+    >
+      <View className="relative  flex-1 px-5 py-5 justify-center items-center bg-background gap-[30px]">
+        <View className="gap-1 items-center">
+          <Text
+            className="font-medium text-h3 text-textPrimary "
+            style={{ fontFamily: "Poppins_500Medium" }}
+          >
+            Sign up
+          </Text>
+          <Text
+            className="text-small text-textPrimary"
+            style={{ fontFamily: "Inter_400Regular" }}
+          >
+            Create an account with your info
+          </Text>
+        </View>
 
-      <View className="gap-3.5 w-full">
-        <TextInputWithIcon
-          value={name}
-          setValue={setName}
-          iconPath={profileIcon}
-          placeholder="Enter full name..."
-        />
-        <TextInputWithIcon
-          value={email}
-          setValue={setEmail}
-          iconPath={emailIcon}
-          placeholder="Enter email..."
-        />
-        <TextInputWithIcon
-          value={phoneNumber}
-          setValue={setPhoneNumber}
-          iconPath={phoneIcon}
-          placeholder="Enter phone number..."
-        />
-      </View>
-
-      <View className="gap-3 items-start w-full">
-        <Text
-          className="text-caption"
-          style={{ fontFamily: "Inter_400Regular", color: "#A9A9A9" }}
-        >
-          Set your password
-        </Text>
-        <View className="gap-3.5 items-start w-full">
+        <View className="gap-3.5 w-full">
           <TextInputWithIcon
-            value={password}
-            setValue={setPassword}
-            iconPath={lockIcon}
-            placeholder="Enter password..."
-            secureTextEntry
+            value={name}
+            setValue={setName}
+            iconPath={profileIcon}
+            placeholder="Enter full name..."
+          />
+          <TextInputWithIcon
+            value={email}
+            setValue={setEmail}
+            iconPath={emailIcon}
+            placeholder="Enter email..."
           />
           <TextInputWithIcon
             value={phoneNumber}
             setValue={setPhoneNumber}
             iconPath={phoneIcon}
-            placeholder="Confirm password..."
-            secureTextEntry
+            placeholder="Enter phone number..."
           />
         </View>
-      </View>
 
-      <Pressable className="w-full" onPress={() => {}}>
-        <View className="bg-primary py-3 px-[18px] justify-center flex-row rounded-[13px]">
+        <View className="gap-3 items-start w-full">
           <Text
-            className="text-buttonText text-surface"
-            style={{ fontFamily: "Inter_500Medium" }}
+            className="text-caption"
+            style={{ fontFamily: "Inter_400Regular", color: "#A9A9A9" }}
           >
-            Sign up
+            Set your password
           </Text>
+          <View className="gap-3.5 items-start w-full">
+            <TextInputWithIcon
+              value={password}
+              setValue={setPassword}
+              iconPath={lockIcon}
+              placeholder="Enter password..."
+              secureTextEntry
+            />
+            <TextInputWithIcon
+              value={phoneNumber}
+              setValue={setPhoneNumber}
+              iconPath={phoneIcon}
+              placeholder="Confirm password..."
+              secureTextEntry
+            />
+          </View>
         </View>
-      </Pressable>
 
-      <View className="absolute flex-row justify-center bottom-16">
-        <Text
-          className="text-bodyBase text-textPrimary"
-          style={{ fontFamily: "Nunito_400Regular" }}
-        >
-          Already have an account?{" "}
-        </Text>
+        <Pressable className="w-full" onPress={() => {}}>
+          <View className="bg-primary py-3 px-[18px] justify-center flex-row rounded-[13px]">
+            <Text
+              className="text-buttonText text-surface"
+              style={{ fontFamily: "Inter_500Medium" }}
+            >
+              Sign up
+            </Text>
+          </View>
+        </Pressable>
 
-        <Pressable onPress={() => router.push("/(auth)/signup")}>
+        <View className=" flex-row justify-center">
           <Text
-            className="text-bodyBase text-primary"
+            className="text-bodyBase text-textPrimary"
             style={{ fontFamily: "Nunito_400Regular" }}
           >
-            Sign in
+            Already have an account?{" "}
           </Text>
-        </Pressable>
+
+          <Pressable onPress={() => router.push("/(auth)/signup")}>
+            <Text
+              className="text-bodyBase text-primary"
+              style={{ fontFamily: "Nunito_400Regular" }}
+            >
+              Sign in
+            </Text>
+          </Pressable>
+        </View>
       </View>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
